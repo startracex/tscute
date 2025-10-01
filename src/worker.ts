b7d442c183f6933069a8f33cfb361dc138e4e818
Shiro Wang <startraceebr@foxmail.com>
2025-10-02 06:24:27 +0800

    fix: resolve IPC error from lost context in process.send

--- src/worker.ts
+++ src/worker.ts
@@ -3,4 +3,4 @@ import { resolve } from "path";
 
 const targetPath = process.argv[2];
 const targetUrl = pathToFileURL(resolve(targetPath)).href;
-import(targetUrl).then(process.send);
+import(targetUrl).then((mod) => process.send(mod));
